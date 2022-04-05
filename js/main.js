207998c25947c531eb12b10fc98d6fc54309e479
Nathan B <brodin@et.esiea.fr>
2022-04-05 18:44:12 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 
-
 /* Change la couleur de la forme au passage de la souris */
-let colorArray = ["#DDE0FA", "#FADDF7", "#FAF2DD", "#FADDDD", "#FAF9DD", "#F6DDFA"];
-const titleShape = document.getElementById("titleShape");
+let colorArray = ["#DDFAE5", "#DDE0FA", "#FADDF7", "#FAF2DD", "#FADDDD", "#FAF9DD", "#F6DDFA"];
 var randomColor;
+
+const titleShape = document.getElementById("title-shape");
+const btnGoto = document.getElementById("goto-portfolio");
+const footer = document.getElementById("footerID");
+const firstProjectShape = document.getElementById("first-project-shape");
 
 titleShape.addEventListener("mouseover", function() {
     randomColor = colorArray[Math.floor(Math.random()*colorArray.length)];
-    titleShape.style.backgroundColor = randomColor;
-});
 
-/*
-titleShape.addEventListener("mouseout", function() {
-    var randomColor = colorArray[Math.floor(Math.random()*colorArray.length)];
     titleShape.style.backgroundColor = randomColor;
+    btnGoto.style.backgroundColor = randomColor;
+    footer.style.backgroundColor = randomColor;
+    firstProjectShape.style.backgroundColor = randomColor;
 });
-*/
