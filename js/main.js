f9198eb8ac4ffeb91ed61f3b42d6cb025cab4507
Nathan B <brodin@et.esiea.fr>
2022-04-08 20:29:18 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ const titleShape = document.getElementById("title-shape");
 const btnGoto = document.getElementById("goto-portfolio");
 const footer = document.getElementById("footerID");
 const firstProjectBg = document.getElementById("first-project-info");
+const secondProjectBg = document.getElementById("second-project-info");
 
 titleShape.addEventListener("mouseover", function() {
     randomColor = colorArray[Math.floor(Math.random()*colorArray.length)];
@@ -15,6 +16,7 @@ titleShape.addEventListener("mouseover", function() {
     btnGoto.style.backgroundColor = randomColor;
     footer.style.backgroundColor = randomColor;
     firstProjectBg.style.backgroundColor = randomColor;
+    secondProjectBg.style.backgroundColor = randomColor;
 });
 
 /* Change les slides des projets */
@@ -46,4 +48,34 @@ function showSlides(n) {
   }
 
   slides[slideIndex-1].style.display = "block";
+}
+
+/* Projet 2 */
+let slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
+}
+
+function currentSlide2(n) {
+  showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
+  let i;
+  let slides2 = document.getElementsByClassName("slide2");
+
+  if (n > slides2.length) {
+      slideIndex2 = 1
+    }
+  if (n < 1) {
+      slideIndex2 = slides2.length
+    }
+
+  for (i = 0; i < slides2.length; i++) {
+    slides2[i].style.display = "none";
+  }
+
+  slides2[slideIndex2-1].style.display = "block";
 }
